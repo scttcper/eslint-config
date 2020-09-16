3964f5074ed05a8d791bd72090e835dcccb36d2d
Scott Cooper <scttcper@gmail.com>
2020-09-15 22:37:32 -0700

    feat: add prettier eslint dependency

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = {
-  extends: ['xo', 'xo-typescript/space'],
+  extends: [
+    'xo',
+    'xo-typescript/space',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'object-curly-spacing': ['error', 'always'],
     'comma-dangle': ['error', 'always-multiline'],
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-unsafe-member-access': 0,
-    '@typescript-eslint/no-unsafe-call': 0,
-    '@typescript-eslint/no-unsafe-return': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-    'capitalized-comments': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    'capitalized-comments': 'off',
   },
 };
