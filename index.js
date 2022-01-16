62f35c58a95a3f29b4ae834b78d466fcbbae907e
Scott Cooper <scttcper@gmail.com>
2022-01-15 22:32:36 -0800

    fix: remove null from ban types

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/naming-convention': 'off',
     'capitalized-comments': 'off',
-
     /**
      * Better import sorting
      */
@@ -55,5 +54,47 @@ module.exports = {
         ],
       },
     ],
+    '@typescript-eslint/ban-types': [
+			'error',
+			{
+				extendDefaults: false,
+				types: {
+					String: {
+						message: 'Use `string` instead.',
+						fixWith: 'string'
+					},
+					Number: {
+						message: 'Use `number` instead.',
+						fixWith: 'number'
+					},
+					Boolean: {
+						message: 'Use `boolean` instead.',
+						fixWith: 'boolean'
+					},
+					Symbol: {
+						message: 'Use `symbol` instead.',
+						fixWith: 'symbol'
+					},
+					Object: {
+						message: 'The `Object` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead. See https://github.com/typescript-eslint/typescript-eslint/pull/848',
+						fixWith: 'Record<string, unknown>'
+					},
+					'{}': {
+						message: 'The `{}` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead.',
+						fixWith: 'Record<string, unknown>'
+					},
+					object: {
+						message: 'The `object` type is hard to use. Use `Record<string, unknown>` instead. See: https://github.com/typescript-eslint/typescript-eslint/pull/848',
+						fixWith: 'Record<string, unknown>'
+					},
+					Function: 'Use a specific function type instead, like `() => void`.',
+					'[]': 'Don\'t use the empty array type `[]`. It only allows empty arrays. Use `SomeType[]` instead.',
+					'[[]]': 'Don\'t use `[[]]`. It only allows an array with a single element which is an empty array. Use `SomeType[][]` instead.',
+					'[[[]]]': 'Don\'t use `[[[]]]`. Use `SomeType[][][]` instead.',
+					'[[[[]]]]': 'ur drunk 🤡',
+					'[[[[[]]]]]': '🦄💥'
+				}
+			}
+		],
   },
 };
