39a9b70bc20b52efbaad79dbcd574b47a53a18ba
Scott Cooper <scttcper@gmail.com>
2022-08-22 15:58:43 -0700

    fix: update dep, disable @typescript-eslint/consistent-type-definitions

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
     'capitalized-comments': 'off',
     'no-warning-comments': 'off',
     /**
@@ -56,46 +57,50 @@ module.exports = {
       },
     ],
     '@typescript-eslint/ban-types': [
-			'error',
-			{
-				extendDefaults: false,
-				types: {
-					String: {
-						message: 'Use `string` instead.',
-						fixWith: 'string'
-					},
-					Number: {
-						message: 'Use `number` instead.',
-						fixWith: 'number'
-					},
-					Boolean: {
-						message: 'Use `boolean` instead.',
-						fixWith: 'boolean'
-					},
-					Symbol: {
-						message: 'Use `symbol` instead.',
-						fixWith: 'symbol'
-					},
-					Object: {
-						message: 'The `Object` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead. See https://github.com/typescript-eslint/typescript-eslint/pull/848',
-						fixWith: 'Record<string, unknown>'
-					},
-					'{}': {
-						message: 'The `{}` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead.',
-						fixWith: 'Record<string, unknown>'
-					},
-					object: {
-						message: 'The `object` type is hard to use. Use `Record<string, unknown>` instead. See: https://github.com/typescript-eslint/typescript-eslint/pull/848',
-						fixWith: 'Record<string, unknown>'
-					},
-					Function: 'Use a specific function type instead, like `() => void`.',
-					'[]': 'Don\'t use the empty array type `[]`. It only allows empty arrays. Use `SomeType[]` instead.',
-					'[[]]': 'Don\'t use `[[]]`. It only allows an array with a single element which is an empty array. Use `SomeType[][]` instead.',
-					'[[[]]]': 'Don\'t use `[[[]]]`. Use `SomeType[][][]` instead.',
-					'[[[[]]]]': 'ur drunk 🤡',
-					'[[[[[]]]]]': '🦄💥'
-				}
-			}
-		],
+      'error',
+      {
+        extendDefaults: false,
+        types: {
+          String: {
+            message: 'Use `string` instead.',
+            fixWith: 'string',
+          },
+          Number: {
+            message: 'Use `number` instead.',
+            fixWith: 'number',
+          },
+          Boolean: {
+            message: 'Use `boolean` instead.',
+            fixWith: 'boolean',
+          },
+          Symbol: {
+            message: 'Use `symbol` instead.',
+            fixWith: 'symbol',
+          },
+          Object: {
+            message:
+              'The `Object` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead. See https://github.com/typescript-eslint/typescript-eslint/pull/848',
+            fixWith: 'Record<string, unknown>',
+          },
+          '{}': {
+            message:
+              'The `{}` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead.',
+            fixWith: 'Record<string, unknown>',
+          },
+          object: {
+            message:
+              'The `object` type is hard to use. Use `Record<string, unknown>` instead. See: https://github.com/typescript-eslint/typescript-eslint/pull/848',
+            fixWith: 'Record<string, unknown>',
+          },
+          Function: 'Use a specific function type instead, like `() => void`.',
+          '[]': "Don't use the empty array type `[]`. It only allows empty arrays. Use `SomeType[]` instead.",
+          '[[]]':
+            "Don't use `[[]]`. It only allows an array with a single element which is an empty array. Use `SomeType[][]` instead.",
+          '[[[]]]': "Don't use `[[[]]]`. Use `SomeType[][][]` instead.",
+          '[[[[]]]]': 'ur drunk 🤡',
+          '[[[[[]]]]]': '🦄💥',
+        },
+      },
+    ],
   },
 };
